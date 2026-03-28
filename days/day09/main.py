7da316aef3492ae085c9f17b3d84a1c1d020a799
Valentin PORCHET <tekrop@proton.me>
2026-03-28 17:55:20 +0100

    fix: day 9 part 1

--- days/day09/main.py
+++ days/day09/main.py
@@ -1,8 +1,10 @@
-from scripts.utils import AbstractPuzzleSolver
-from itertools import combinations
 from collections import namedtuple
-from functools import cache
-from rich import print
+from dataclasses import dataclass
+from functools import cached_property
+from itertools import combinations
+from typing import Any
+
+from scripts.utils import AbstractPuzzleSolver
 
 
 class PuzzleSolver(AbstractPuzzleSolver):
@@ -10,90 +12,85 @@ class PuzzleSolver(AbstractPuzzleSolver):
     # DAY 09 - Common Part
     ###########################
     def solve(self) -> tuple[int, int]:
-        self.red_tiles: list[Position] = [
-            Position(*(int(coord) for coord in line.split(","))) for line in self.lines
-        ]
-        return super().solve()
+        self.red_tiles: list[Position] = []
+        for line in self.lines:
+            pos_x, pos_y = line.split(",")
+            self.red_tiles.append(Position(int(pos_x), int(pos_y)))
 
-    @cache
-    def _get_rectangle_area(self, red_tile_pair: tuple[Position, Position]) -> int:
-        width = abs(red_tile_pair[0].x - red_tile_pair[1].x)
-        height = abs(red_tile_pair[0].y - red_tile_pair[1].y)
-        return (width + 1) * (height + 1)
+        self.rectangles: list[Rectangle] = [
+            Rectangle(red_tile_pair)
+            for red_tile_pair in combinations(self.red_tiles, 2)
+        ]
+        self.rectangles.sort(reverse=True)
+
+        return super().solve()
 
     ###########################
     # DAY 09 - First Part
     ###########################
     def _solve_first_part(self) -> int:
-        return max(
-            self._get_rectangle_area(red_tile_pair)
-            for red_tile_pair in combinations(self.red_tiles, 2)
-        )
+        return next(iter(self.rectangles)).area
 
     ###########################
     # DAY 09 - Second Part
     ###########################
 
     def _solve_second_part(self) -> int:
-        self._compute_grid()
-        return max(
-            self._get_rectangle_area(red_tile_pair)
-            for red_tile_pair in combinations(self.red_tiles, 2)
-            if self._is_valid_rectangle(red_tile_pair)
-        )
-
-    def _compute_grid(self) -> None:
-        # Loop over to assign green tiles
-        self.green_and_red_tiles = set()
-        for i in range(-1, len(self.red_tiles) - 1):
-            current_tile = self.red_tiles[i]
-            next_tile = self.red_tiles[i + 1]
-            # print(red_tile)
-            # print(next_tile)
-            # print("---")
-            self.green_and_red_tiles |= {
-                Position(x, y)
-                for x in (
-                    range(
-                        current_tile.x,
-                        next_tile.x,
-                        int(
-                            (next_tile.x - current_tile.x)
-                            / abs(next_tile.x - current_tile.x)
-                        ),
-                    )
-                    if next_tile.x != current_tile.x
-                    else [current_tile.x]
-                )
-                for y in (
-                    range(
-                        current_tile.y,
-                        next_tile.y,
-                        int(
-                            (next_tile.y - current_tile.y)
-                            / abs(next_tile.y - current_tile.y)
-                        ),
-                    )
-                    if next_tile.y != current_tile.y
-                    else [current_tile.y]
-                )
-            }
-
-        print(len(self.green_and_red_tiles))
-        # print(self.green_and_red_tiles)
-
-        min_x = min(red_tile.x for red_tile in self.red_tiles)
-        max_x = max(red_tile.x for red_tile in self.red_tiles)
-        min_y = min(red_tile.y for red_tile in self.red_tiles)
-        max_y = max(red_tile.y for red_tile in self.red_tiles)
-
-        print(min_x, max_x)
-        print(min_y, max_y)
-        # Compute green positions
-
-    def _is_valid_rectangle(self, pair: tuple[Position, Position]) -> bool:
-        # Check every coordinates
-        return True
+        return 0
 
 
 Position = namedtuple("Position", ["x", "y"])
+
+
+@dataclass
+class Rectangle:
+    """Rectangle representation class for the challenge.
+
+    We're keeping track of the red tile pair, but also defining a few things :
+    - Reusable metrics : width and height of the rectangle, min and max positions
+    - Comparison with others rectangles using the area
+    """
+
+    red_tile_pair: tuple[Position, Position]
+
+    @cached_property
+    def min(self) -> Position:
+        return Position(
+            min(self.red_tile_pair[0].x, self.red_tile_pair[1].x),
+            min(self.red_tile_pair[0].y, self.red_tile_pair[1].y),
+        )
+
+    @cached_property
+    def max(self) -> Position:
+        return Position(
+            max(self.red_tile_pair[0].x, self.red_tile_pair[1].x),
+            max(self.red_tile_pair[0].y, self.red_tile_pair[1].y),
+        )
+
+    @cached_property
+    def width(self) -> int:
+        return self.max.x - self.min.x + 1
+
+    @cached_property
+    def height(self) -> int:
+        return self.max.y - self.min.y + 1
+
+    @cached_property
+    def area(self) -> int:
+        return self.width * self.height
+
+    def __lt__(self, other: "Rectangle") -> bool:
+        return self.area < other.area
+
+    def __eq__(self, other: Any) -> bool:
+        return self.area == other.area
+
+    def __repr__(self) -> str:
+        return (
+            "Rectangle("
+            f"area={self.area}, "
+            f"width={self.width}, "
+            f"height={self.height}, "
+            f"pair={self.red_tile_pair}"
+            ")"
+        )
