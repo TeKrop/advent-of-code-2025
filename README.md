2e88fcd47e3b3c148b0ed95c4a026ed9e1c24270
Valentin Porchet <tekrop@proton.me>
2026-03-31 23:05:46 +0200

    updated day 11 part 1

--- README.md
+++ README.md
@@ -50,20 +50,19 @@ If --submit is used, solution will be submitted on AoC website using your AOC_SE
 
 ## 🎯 Progress
 
-| Day   | Part 1 | Part 2 |
-|-------|--------|--------|
-| Day 1 |   ⭐   |   ⭐   |
-| Day 2 |   ⭐   |   ⭐   |
-| Day 3 |   ⭐   |   ⭐   |
-| Day 4 |   ⭐   |   ⭐   |
-| Day 5 |   ⭐   |   ⭐   |
-| Day 6 |   ⭐   |   ⭐   |
-| Day 7 |   ⭐   |   ⭐   |
-| Day 8 |   ⭐   |   ⭐   |
-| Day 9 |   ⭐   |        |
-| Day 10 |   ⭐   |        |
-| Day 11 |   ⭐   |        |
+| Day    | Part 1 | Part 2 |
+|--------|--------|--------|
+| Day 1  |   ⭐    |   ⭐    |
+| Day 2  |   ⭐    |   ⭐    |
+| Day 3  |   ⭐    |   ⭐    |
+| Day 4  |   ⭐    |   ⭐    |
+| Day 5  |   ⭐    |   ⭐    |
+| Day 6  |   ⭐    |   ⭐    |
+| Day 7  |   ⭐    |   ⭐    |
+| Day 8  |   ⭐    |   ⭐    |
+| Day 9  |   ⭐    |        |
+| Day 10 |   ⭐    |        |
+| Day 11 |   ⭐    |        |
 | Day 12 |        |        |
 
 *Legend: ⭐ = Star won (solution implemented)*
-
--- days/day11/main.py
+++ days/day11/main.py
@@ -21,27 +21,26 @@ class PuzzleSolver(AbstractPuzzleSolver):
     def _solve_first_part(self) -> int:
         self.start: str = "you"
         self.end: str = "out"
+        self.nb_out = 0
 
-        devices, nb_out = [self.start], 0
+        devices: list[str] = [self.start]
 
         while len(devices) > 0:
-            devices, nb_out = self._process_output_devices(devices, nb_out)
+            devices = self._process_output_devices(devices)
 
-        return nb_out
+        return self.nb_out
 
-    def _process_output_devices(
-        self, input_devices: list[str], nb_out: int
-    ) -> tuple[list[str], int]:
+    def _process_output_devices(self, input_devices: list[str]) -> list[str]:
         output_devices: list[str] = []
 
         for input_device in input_devices:
             for output_device in self.devices[input_device]:
                 if output_device == self.end:
-                    nb_out += 1
+                    self.nb_out += 1
                 else:
                     output_devices.append(output_device)
 
-        return output_devices, nb_out
+        return output_devices
 
     ###########################
     # DAY 11 - Second Part
